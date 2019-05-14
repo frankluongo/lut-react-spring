9dbe4e43f40a9a857437a4042cb9b8ebb666101c
Frank Luongo <frankluongo92@gmail.com>
2019-05-13 20:20:21 -0400

    Emulating Keyframes

--- src/Components/Toggle.js
+++ src/Components/Toggle.js
@@ -18,13 +18,16 @@ const Toggle = () => {
     color: isToggled ? '#000' : 'rgba(255,255,255,0.5)',
     fontSize: isToggled ? '1rem' : '3em',
     // transform: isToggled ? 'translate3d(0,0,0)' : 'translate3d(0, -50px, 0)',
-    y: isToggled ? 0 : -50
+    y: isToggled ? 0 : 1
   });
 
   return (
     <div>
       <AnimatedTitle style={{
-        transform: y.interpolate(y => `translate3d(0,${y}px,0)`),
+        transform: y.interpolate({
+          range: [0, 0.25, 0.50, 0.75, 1],
+          output: [0, -25, -50. -100, -50]
+        }).interpolate(y => `translate3d(0,${y}px,0)`),
         color
         }}>Hello</AnimatedTitle>
       <button onClick={() => setToggle(!isToggled)}>Toggle</button>
