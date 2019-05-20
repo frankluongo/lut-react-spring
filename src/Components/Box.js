d8814af38acd180655e21ca85992ae4ce226230b
Frank Luongo <frank.luongo@revzilla.com>
2019-05-20 13:19:45 -0400

    18: Gestures with events

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -5,18 +5,33 @@ import { animated, useSpring } from 'react-spring'
 
 
 const Box = () => {
-  const [{xy}, set] = useSpring(() => ({
-      xy: [0, 0]
+  const [{x}, set] = useSpring(() => ({
+      x: 0
     }));
 
   const bind = useGesture ( ({ down, delta }) => {
-    set({ xy: down ? delta : [0,0] });
+
+    if (down) {
+      set({ x: down ? delta[0] : 0});
+    } else {
+      if (delta[0] > 400) {
+        set({ x: 500})
+      } else {
+        set({ x: 0})
+      }
+    }
+
   })
 
   return (
     <animated.div
       style={{
-        transform: xy.interpolate((x,y) => `translate3d(${x}px, ${y}px, 0)`)
+        opacity: x.interpolate({
+          map: Math.abs,
+          range: [0, 400],
+          output: [1, 0.5]}
+        ),
+        transform: x.interpolate((x) => `translate3d(${x}px, 0, 0)`)
       }}
       {...bind()}
       className="box" />
